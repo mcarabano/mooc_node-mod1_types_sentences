23cd71da0a00386088ac4c920dae63a0a8cb09fa
mcarabano <47810572+mcarabano@users.noreply.github.com>
2019-03-22 14:48:45 +0100

    Update mod1_types_sentences.js

--- mod1_types_sentences.js
+++ mod1_types_sentences.js
@@ -1,1 +1,19 @@
-// Type your code here
+console.log();
+var hora = new Date().getHours();
+var saludo;
+
+if (hora>=7 && hora<13) {
+	saludo =  "Good morning";
+} else if (hora>=13 && hora<22) {
+	saludo =  "Good afternoon";
+} else if (hora>=22 && hora<7) {
+	saludo =  "Good night";
+}
+console.log(`${saludo}, it's ${hora}  o´clock`);
+console.log();
+console.log ("Number Pi with 6 decimals: " + Math.PI.toFixed(6));
+console.log();
+var linea =  x => x + " dec = " + x.toString(16) + " hex = " + x.toString(8) + " oct = " + x.toString(2) + " bin";  
+for (var i=0;i<23;i++) {
+	console.log(`${linea(i)}`);
+}
